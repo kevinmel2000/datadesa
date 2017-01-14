3ff7691e27ca4095f523502e716279477cca96ad
prianthon <prianthonsubardio@gmail.com>
2017-01-14 17:08:48 +0700

    front ed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Data Desa</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -68,21 +68,21 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
+                    <a href="{{ url('/login') }}">Masuk</a>
+                    <a href="{{ url('/register') }}">Daftar</a>
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Data Desa
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://laravel.com/docs">Dokumentasi</a>
+                    <a href="https://laracasts.com">Datacasts</a>
+                    <a href="https://laravel-news.com">Kabar Desa</a>
+                    <a href="https://forge.laravel.com">Datacloud</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
