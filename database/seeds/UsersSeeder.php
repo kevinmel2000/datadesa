fc5c0fe4a32315a228d14156326360fac6b0ac71
prianthon <prianthonsubardio@gmail.com>
2017-01-14 22:38:50 +0700

    sample admin

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Membuat sample admin
+        $admin = new User();
+        $admin->name = 'Admin Data Desa';
+        $admin->email = 'info@prianthon.id';
+        $admin->password = bcrypt('merdeka1945');
+        $admin->save();
+    }
+}
