ff15e694bbe731ebd36f9a51d69e3f9df887095a
prianthon <prianthonsubardio@gmail.com>
2017-01-14 21:09:13 +0700

    macro blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      require base_path() . '/app/Helpers/frontend.php';
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,9 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                      @if (Auth::check())
+                        {!! Html::smartNav(url('/home'), 'Dashboard') !!}
+                      @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
